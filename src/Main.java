c880045e000d12215754a69d71288bf42cef277c
Vladislav Khomchenkov <vladdiablo1@mail.ru>
2020-07-19 12:59:24 +0300

    class

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        Collections.sort(intList);
+        for (int x : intList) {
+            if (x <= 0) continue;
+            if (x % 2 != 0) continue;
+            System.out.println(x);
+        }
+    }
+}
